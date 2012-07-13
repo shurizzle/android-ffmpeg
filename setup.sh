2e3bed3f48d0ae363035664c65fc82f33f78a547
shura <shura1991@gmail.com>
2012-07-13 10:25:17 +0200

    add compile patch

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ wget http://ffmpeg.org/releases/ffmpeg-0.8.12.tar.bz2
 tar xvf ffmpeg-0.8.12.tar.bz2
 mv ffmpeg-0.8.12 ffmpeg
 rm ffmpeg-0.8.12.tar.bz2
+patch -d ffmpeg -p1 -i ../ffmpeg-arm11.patch
 wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
 tar xvf lame-3.99.5.tar.gz
 mv lame-3.99.5 lame
